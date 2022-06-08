ea5e252b59844392e0777e4bbadfa34eefd97552
Ioana Ciornei <ioana.ciornei@nxp.com>
2022-06-08 15:27:42 +0300

    dpaa2-switch: say that the bond device is offloaded as a bridge port
    commit  d2b0b48b337eecc55f26217c2a7215bc84d99376 from
    https://github.com/nxp-imx/linux-imx lf-6.1.y
    
    We have to let the switchdev framework know if a specific bridge port is
    offloaded or not, even if that port is bond device.
    For this to happen, create the dpaa2_switch_port_to_bridge_port function
    which will determine the bridge port corresponding to a particulat dpaa2
    switch interface and use it in the switchdev_bridge_port_offload call.
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/net/ethernet/freescale/dpaa2/dpaa2-switch.c
+++ drivers/net/ethernet/freescale/dpaa2/dpaa2-switch.c
@@ -2104,6 +2104,17 @@ static int dpaa2_switch_port_attr_set_event(struct net_device *netdev,
 	return notifier_from_errno(err);
 }
 
+static struct net_device *dpaa2_switch_port_to_bridge_port(struct ethsw_port_priv *port_priv)
+{
+	if (!port_priv->fdb->bridge_dev)
+		return NULL;
+
+	if (port_priv->lag)
+		return port_priv->lag->bond_dev;
+
+	return port_priv->netdev;
+}
+
 static int dpaa2_switch_port_bridge_join(struct net_device *netdev,
 					 struct net_device *upper_dev,
 					 struct netlink_ext_ack *extack)
@@ -2112,7 +2123,7 @@ static int dpaa2_switch_port_bridge_join(struct net_device *netdev,
 	struct ethsw_core *ethsw = port_priv->ethsw_data;
 	struct dpaa2_switch_fdb *old_fdb = port_priv->fdb;
 	struct ethsw_port_priv *other_port_priv;
-	struct net_device *other_dev;
+	struct net_device *other_dev, *brport_dev;
 	struct list_head *iter;
 	bool learn_ena;
 	int err;
@@ -2151,7 +2162,8 @@ static int dpaa2_switch_port_bridge_join(struct net_device *netdev,
 	if (err)
 		goto err_egress_flood;
 
-	err = switchdev_bridge_port_offload(netdev, netdev, port_priv,
+	brport_dev = dpaa2_switch_port_to_bridge_port(port_priv);
+	err = switchdev_bridge_port_offload(brport_dev, netdev, port_priv,
 					    NULL, NULL, false, extack);
 	if (err)
 		goto err_switchdev_offload;
@@ -2186,7 +2198,12 @@ static int dpaa2_switch_port_restore_rxvlan(struct net_device *vdev, int vid, vo
 
 static void dpaa2_switch_port_pre_bridge_leave(struct net_device *netdev)
 {
-	switchdev_bridge_port_unoffload(netdev, NULL, NULL, NULL);
+	struct ethsw_port_priv *port_priv = netdev_priv(netdev);
+	struct net_device *brport_dev;
+
+	brport_dev = dpaa2_switch_port_to_bridge_port(port_priv);
+
+	switchdev_bridge_port_unoffload(brport_dev, port_priv, NULL, NULL);
 }
 
 static int dpaa2_switch_port_bridge_leave(struct net_device *netdev)
